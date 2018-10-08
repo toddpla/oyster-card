ab80ae4ea5d82260dd965f4d207674eace864cb3
Todd Langford-Archer <toddlangford-archer@Todds-MacBook-Pro.local>
2018-10-08 14:39:02 +0100

    step 4 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe OysterCard do
-  
+
+  describe '#balance' do
+    it 'a new card should have a defualt balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
